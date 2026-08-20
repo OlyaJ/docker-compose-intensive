fc3c217866a8a7130f99a84a8b195e565b8a89bb
OlyaJ <schastnayaolga92@gmail.com>
2026-08-20 21:44:56 +0300

    fixed changes

--- backend/server.js
+++ backend/server.js
@@ -91,5 +91,5 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`🚀 Сервер запущен на порту ${PORT}`);
   console.log(`📊 API доступен по адресу: http://localhost:${PORT}/api`);
-  console.log('api version2');
+  console.log('api version2.2');
 });
